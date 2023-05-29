a0747aff661f7675ab0986ab0cf1acba30ce1a85
JongSooKim <prjs3643@gmail.com>
2023-05-29 14:40:09 +0900

    메타데이터를 공통클래스로 추출

--- src/main/java/com/study/projectboard/domain/Article.java
+++ src/main/java/com/study/projectboard/domain/Article.java
@@ -22,9 +22,8 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -44,19 +43,7 @@ public class Article {
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
 
-    //TODO : 공통 메타데이터를 BaseEntity로 공통화
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; // 생성일자
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; // 생성자
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; // 수정일자
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; // 수정자
+
 
     public Article(String title, String content, String hashtag) {
         this.title = title;
